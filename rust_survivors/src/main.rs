280c4644763f1939c90da9d25f5f85110d084816
Henry Meiners <henry.meiners@slu.edu>
2023-10-20 10:39:38 -0500

    changed 3d to 2d sprites to better reflect project

--- rust_survivors/src/main.rs
+++ rust_survivors/src/main.rs
@@ -1,64 +1,201 @@
-use bevy::{ math::vec2, math::vec3, 
-    prelude::*, 
-    sprite::collide_aabb::*};
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-
-//player vars
-const PLAYER_SIZE: Vec2 = Vec2::new(200.0, 200.0);
-const PLAYER_SPEED: f32 = 500.0;
-
+//scene const variables
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
 
 fn main() {
 App::new()
-.insert_resource(ClearColor(Color::rgb(0.2,0.2,0.2)))
-
-.add_plugins(DefaultPlugins.set(WindowPlugin {
-    primary_window: Some(Window {
-        resolution: (WIDTH, HEIGHT).into(),
-        title: "Rust Survivors".to_string(),
-        resizable: false,
+    //window setup and default plugins
+    .insert_resource(ClearColor(Color::rgb(0.2,0.2,0.2)))
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            resolution: (WIDTH, HEIGHT).into(),
+            title: "Rust Survivors".to_string(),
+            resizable: false,
+            ..default()
+        }),
         ..default()
-    }),
-    ..default()
-}))
+    }))
 
-.add_systems(Startup, spawn_camera)
-.add_systems(Startup, spawn_basic_scene)
-//.add_systems(Startup, setup)
+    //inspector setup
+    .add_plugins(WorldInspectorPlugin::new())
+    .register_type::<Enemy>()
+    .register_type::<Lifetime>()
 
-//.add_systems(Update, bevy::window::close_on_esc)
+    //startup systems
+    //.add_systems(Startup, asset_loading)
+    .add_systems(Startup, spawn_camera)
+    .add_systems(Startup, spawn_basic_scene)
 
-//.add_systems(FixedUpdate, (move_player, player_check_collisions))
-//.add_systems(FixedUpdate, player_lose_health)
-.run();
+    //FixedUpdate systems
+    .add_systems(FixedUpdate, sprite_movement)
+    //.add_systems(FixedUpdate, enemy_shooting)
+    //.add_systems(FixedUpdate, bullet_despawn)
+ 
+    .run();
 }
 
 
 fn spawn_camera(mut commands: Commands) {
-commands.spawn(Camera3dBundle {
-transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-..default()
-});
+    commands.spawn(
+        Camera2dBundle::default())
+        .insert(Camera);
+
 }
 
 fn spawn_basic_scene(
-mut commands: Commands,
-mut meshes: ResMut<Assets<Mesh>>,
-mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
 ) {
-//spawn plane
-commands.spawn(PbrBundle {
-mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0, subdivisions: 0 })),
-material: materials.add(Color::rgb(0.3, 0.4, 0.3).into()),
-..default()
-});
-//spawn cube
-commands.spawn(PbrBundle {
-mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
-transform: Transform::from_xyz(0.0, 0.5, 0.0),
-..default()
-});
+    //spawn enemy
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("slime.png"),
+            transform: Transform::from_xyz(100.0, 0.0, 0.0),
+            ..default()
+        },
+        Direction2D {
+            vertical: Direction::Up,
+            horizontal: Direction::Left
+        },
+        Enemy {
+            shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        },
+        Name::new("Slime"),
+    ));
+
+    //spawn player
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("poe_0.png"),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+        Player,
+        Name::new("Player 1"),
+    ));
 }
+
+fn sprite_movement(
+    time: Res<Time>,
+    mut sprite_position: Query<(&mut Direction2D, &mut Transform)>
+) {
+    for(mut direction_bundle, mut transform) in  &mut sprite_position {
+        match direction_bundle.vertical {
+            Direction::Up           => transform.translation.y += 150.0 * time.delta_seconds(),
+            Direction::Down         => transform.translation.y -= 150.0 * time.delta_seconds(),
+            Direction::Nonmoving    => transform.translation.y = 0.0,
+            _                       => panic!("you cant have a horizontal direction in the vertical"),
+        }
+
+        match direction_bundle.horizontal {
+            Direction::Right        => transform.translation.x += 150.0 * time.delta_seconds(),
+            Direction::Left         => transform.translation.x -= 150.0 * time.delta_seconds(),
+            Direction::Nonmoving    => transform.translation.x = 0.0,
+            _                      => panic!("you cant have a vertical direction in the horizontal"),
+        }
+
+        if transform.translation.y > 200.0 {
+            direction_bundle.vertical = Direction::Down;
+        } else if transform.translation.y < -200.0 {
+            direction_bundle.vertical = Direction::Up;
+        }
+
+        if transform.translation.x > 200.0 {
+            direction_bundle.horizontal = Direction::Left;
+        } else if transform.translation.x < -200.0 {
+            direction_bundle.horizontal = Direction::Right;
+        }
+    }
+}
+
+
+
+fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.insert_resource(GameAssets {
+        bullet_scene: assets.load("Bullet.glb#Scene0"),
+    });
+}
+
+
+fn enemy_shooting(
+    mut commands: Commands,
+    bullet_assets: Res<GameAssets>,
+    mut enemies: Query<&mut Enemy>,
+    time: Res<Time>
+) {
+    for mut enemy in &mut enemies {
+        enemy.shooting_timer.tick(time.delta());
+        if enemy.shooting_timer.just_finished() {
+            //initial transform position of bullet
+            let spawn_transform =
+                Transform::from_xyz(0.0, 0.7, 0.6)
+                .with_rotation(Quat::from_rotation_y(-std::f32::consts::PI / 2.0));
+            
+            //spawn bullet
+            commands.spawn(SceneBundle {
+                scene: bullet_assets.bullet_scene.clone(),
+                transform: spawn_transform,
+                ..Default::default()
+            })
+            .insert(Lifetime {
+                timer: Timer::from_seconds(0.5, TimerMode::Once)
+            })
+            .insert(Name::new("Bullet"));
+        }
+    }
+}
+
+fn bullet_despawn(
+    mut commands: Commands,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+    time: Res<Time>
+) {
+    for (entity, mut lifetime) in &mut bullets {
+        lifetime.timer.tick(time.delta());
+        if lifetime.timer.just_finished() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+#[derive(Component)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    Nonmoving,
+}
+
+#[derive(Component)]
+pub struct Direction2D {
+    vertical: Direction,
+    horizontal: Direction,
+}
+
+
+#[derive(Resource)]
+pub struct GameAssets {
+    bullet_scene: Handle<Scene>,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Enemy {
+    shooting_timer: Timer,
+}
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    timer: Timer,
+}
+
+#[derive(Component)]
+pub struct Camera;
